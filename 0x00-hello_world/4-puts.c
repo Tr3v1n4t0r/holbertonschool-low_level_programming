de6ddfca3d48714183614d19699e7daa1b3766e3
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-19 20:39:19 +0000

    4 commit 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ *main - void
+ *
+ *Descriptions: This function prints the string that I need
+ *Return: value of 0
+ */
 int main(void)
 {
 char string[50];
