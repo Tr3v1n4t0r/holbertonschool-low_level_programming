100b9a3a16a6e70f9f096a4984571e0c99f6864f
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-19 22:09:41 +0000

    task 4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,8 +9,6 @@
  */
 int main(void)
 {
-char string[51];
-strcpy(string,"\"Programming is like building a multilingual puzzle");
-puts(string);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
