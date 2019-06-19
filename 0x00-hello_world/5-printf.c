4d7b1934317fb83963fb31ddcfbca4a03873a656
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-19 23:13:26 +0000

    task 5 commit 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  *Return: value of 0
  */
 int main(void)
-{	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+{	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
