e7aba78afa5235fb92fd02d0d24c0fd62b0ceab0
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-19 20:12:02 +0000

    4 commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,7 @@
-puts("Programming is like building a multilingual puzzle")
+int main(void)
+{
+char string[50];
+strcpy(str, "\"Programming is like building a multilingual puzzle");
+puts(string);
+return (0);
+}
