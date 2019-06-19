f59817c4f63449fda39a7d1cf22725ebdf8fb938
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-19 22:37:22 +0000

    task 5 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ *main - void
+ *
+ *Descriptions: This function prints the string that I need
+ *Return: value of 0
+ */
+int main(void)
+{	printf("\with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
