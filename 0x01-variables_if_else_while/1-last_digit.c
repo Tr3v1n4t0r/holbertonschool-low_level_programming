06131957e877f8fcef0e194185ed492d27ed5bc0
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-20 19:43:47 +0000

    task 1 commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	{
 		printf("and is greater than 5");
 	}
-	if (n == 0)
+	else if (n < 6 && n != 0)
 	{
 		printf("and is 0");
 	}
-	if (n < 6 && n != 0)
+	else
 	{
 		printf("and is less than 6 and not 0\n");
 	}
