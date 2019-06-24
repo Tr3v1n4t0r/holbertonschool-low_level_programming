f2ff55a3d6bd2e80b578e30271a03fbf78cf4c77
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-24 21:46:22 +0000

    fixing everything

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,16 +7,11 @@
  */
 int main(void)
 {
-	_putchar ('h');
-        _putchar ('o');
-        _putchar ('l');
-        _putchar ('b');
-        _putchar ('e');
-        _putchar ('r');
-        _putchar ('t');
-        _putchar ('o');
-        _putchar ('n');
-        _putchar ('\n');
+	char *w = "Holberton\n";
 
+	while (*w)
+	{
+		_putchar (*w++);
+	}
 	return (0);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,1 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
 int _putchar(char c);
-
-#endif
