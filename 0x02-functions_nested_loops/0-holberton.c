005ca6621bd287ffca8ba500378e15401099ad65
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-24 19:58:37 +0000

    made a .h file and got 0 working correctly

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  *main - Entry point
@@ -7,8 +7,16 @@
  */
 int main(void)
 {
-	printf("Holberton");
-	putchar('\n');
+	_putchar ('h');
+        _putchar ('o');
+        _putchar ('l');
+        _putchar ('b');
+        _putchar ('e');
+        _putchar ('r');
+        _putchar ('t');
+        _putchar ('o');
+        _putchar ('n');
+        _putchar ('\n');
 
 	return (0);
 }
