d4da93105399389a648c19e3ba9918194d8ce7b4
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-25 15:05:58 +0000

    removed the return in file one and fixed the main funcion

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- *main - check the code for Holberton School students.
+ *print_alphabet - check the code for Holberton School students.
  *
  *Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char s;
 
 	for (s = 'a' ; s <= 'z' ; s++)
 		_putchar (s);
+
 	_putchar ('\n');
-	return (0);
 }
