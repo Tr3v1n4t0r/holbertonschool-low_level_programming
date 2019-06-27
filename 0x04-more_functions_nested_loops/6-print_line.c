1d31c89bd8d182cb6cec0134b637d0bd4d37d161
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-27 20:15:21 +0000

    completed task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_line - Prints a straight line
+ *
+ *@n: int
+ *
+ *Return: 0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar ('\n');
+	else
+	{
+		while (n >= 0)
+		{
+			_putchar ('_');
+			n--;
+		}
+		_putchar ('\n');
+	}
+}
