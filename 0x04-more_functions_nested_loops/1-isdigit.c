0390313100de5a41aa342faf83475a0f11844bec
Tr3v1n4t0r <813@holbertonschool.com>
2019-06-27 16:17:17 +0000

    completed task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_isdigit - Checks for digits
+ *
+ *@c: int
+ *
+ *Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
