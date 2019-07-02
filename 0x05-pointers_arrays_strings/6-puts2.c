2dcec8ba72d63a6084c7ab31efbdb5630e6c13a9
Tr3v1n4t0r <813@holbertonschool.com>
2019-07-02 21:36:39 +0000

    task 6 fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,32 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int x;
+
+	for (x = 0 ; x < _strlen(str) ; str += 2)
 	{
-		_putchar (*str);
-		str = str + 2;
+		_putchar (str[x]);
 	}
 	_putchar ('\n');
+}
+
+#include "holberton.h"
+
+/**
+ *_strlen - returns the length of a string
+ *
+ *@s: int
+ *
+ *Return: 0
+ */
+int _strlen(char *s)
+{
+        int a = 0;
+
+        while (*s != '\0')
+        {
+                s++;
+                a++;
+        }
+        return (a);
 }
