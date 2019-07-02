a0402ab91e122f64a211d6885fe787cf2d658506
Tr3v1n4t0r <813@holbertonschool.com>
2019-07-02 21:26:59 +0000

    task 8 made betty happy

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,13 @@
 void print_array(int *a, int n)
 {
 	int x;
-	for (x = 0; x < n ; x++)
+
+	for (x = 0 ; x < n ; x++)
 	{
 		if (x != 0)
-			printf (", %d", *(a + x));
+			printf(", %d", *(a + x));
 		else
-			printf ("%d", *(a + x));
+			printf("%d", *(a + x));
 	}
-	printf ("\n");
+	printf("\n");
 }
