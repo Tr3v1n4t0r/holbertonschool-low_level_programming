e4543cbba8d76fa37c58878cf5295c30c19fbc5c
Tr3v1n4t0r <813@holbertonschool.com>
2019-07-02 17:32:51 +0000

    task 4 completed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_rev - prints a string in reverse
+ *
+ *@s: string
+ *
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+	int  count, a;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	for (a = count ; a >= 0 ; a--, s--)
+		_putchar (*s);
+	_putchar ('\n');
+}
