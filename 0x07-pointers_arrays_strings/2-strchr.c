258c3831bacb9a9718bf92dd61b29ae605dd5d46
vagrant <vagrant@precise64.(none)>
2019-07-10 02:12:18 +0000

    taks 2 completed

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_strchr - locates a character
+ *
+ *@s: string
+ *@c: char
+ *
+ *Return: a pointer to char c in string s, or NULL in no char
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; ++s)
+		if (*s == c)
+			return (s);
+
+	return ('\0');
+}
