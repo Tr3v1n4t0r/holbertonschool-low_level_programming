1288adac209dd506c94a754fdd3a04fec2ec7d80
vagrant <vagrant@precise64.(none)>
2019-07-12 03:09:27 +0000

    task 1 completed

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - prints a string in reverse
+ *
+ *@s: string
+ *
+ *Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+}
