0da84db94c78fae2196ab133124976288fed4559
Tr3v1n4t0r <813@holbertonschool.com>
2019-07-17 15:23:54 +0000

    task 1 complete

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  *_strdup - returns a pointer to a newly allocated space in memory
@@ -9,5 +10,24 @@
  */
 char *_strdup(char *str)
 {
+	char *space;
+	unsigned int i, length;
 
+	if (str == NULL)
+		return (NULL);
+
+	length = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	space = malloc(sizeof(char) * (length + 1));
+
+	if (space == NULL)
+		return (NULL);
+
+	for (i = 0; i < length + 1; i++)
+		*(space + i) = *(str + i);
+
+	return (space);
 }
