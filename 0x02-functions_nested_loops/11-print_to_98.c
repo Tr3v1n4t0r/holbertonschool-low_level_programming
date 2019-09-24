bfd77863937f9f469d11375a4f437fbf159f9f78
Tr3v1n4t0r <813@holbertonschool.com>
2019-09-24 11:27:46 -0700

    made betty happy but made my code ugly

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,30 +10,21 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 		_putchar('9');
-		_putchar('8');
-	}
+		_putchar('8'); }
 	else if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
 			if (n > 99)
-			{
-				_putchar(n / 100 + '0');
+			{_putchar(n / 100 + '0');
 				_putchar(((n % 100) / 10) + '0');
-				_putchar(n % 10 + '0');
-			}
+				_putchar(n % 10 + '0');	}
 			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
+			{_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');	}
 			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
+			{_putchar(',');
+				_putchar(' '); }}}
 	else
 	{
 		for (; n <= 98; n++)
@@ -41,32 +32,18 @@ void print_to_98(int n)
 			if (n != 98)
 			{
 				if (n < 0)
-				{
-					_putchar('-');
+				{_putchar('-');
 					if (-n / 10 != 0)
-					{
 						_putchar(-n / 10 + '0');
-					}
-					_putchar(-n % 10 + '0');
-				}
+					_putchar(-n % 10 + '0'); }
 				else if (n < 10)
-				{
 					_putchar(n + '0');
-				}
 				else
-				{
-					_putchar(n / 10 + '0');
-					_putchar(n % 10 + '0');
-				}
+				{_putchar(n / 10 + '0');
+					_putchar(n % 10 + '0');	}
 				_putchar(',');
-				_putchar(' ');
-			}
+				_putchar(' '); }
 			else
-			{
-				_putchar('9');
-				_putchar('8');
-			}
-		}
-	}
-	_putchar('\n');
-}
+			{_putchar('9');
+				_putchar('8'); }}}
+	_putchar('\n'); }
