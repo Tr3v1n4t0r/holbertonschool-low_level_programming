71ed2cb590c5fd7d32f7a1abf7ae4a62b97556cc
Tr3v1n4t0r <813@holbertonschool.com>
2019-09-24 11:21:19 -0700

    fixed three digit numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,13 +19,18 @@ void print_to_98(int n)
 			if (n > 99)
 			{
 				_putchar(n / 100 + '0');
-				_putchar(n / 10 + '0');
+				_putchar(((n % 100) / 10) + '0');
 				_putchar(n % 10 + '0');
 			}
 			else
 			{
 				_putchar(n / 10 + '0');
 				_putchar(n % 10 + '0');
+			}
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
