32a263fe4d3069a3cb864cbb4a5034be0cb0c08c
Tr3v1n4t0r <813@holbertonschool.com>
2019-09-24 12:33:42 -0700

    made betty happy

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,42 +8,40 @@
 void print_times_table(int n)
 {
 	int a, b;
+
 	if (n > 15 || n < 0)
 		return;
-	else
+	for (a = 0; a <= n; a++)
 	{
-		for (a = 0; a <= n; a++)
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
+			if (b == 0)
 			{
-				if (b == 0)
+				_putchar('0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				if ((a * b) < 100)
+					_putchar(' ');
+				if ((a * b) >= 10 && (a * b) < 100)
+					_putchar(a * b / 10 + '0');
+				else if ((a * b) >= 100)
 				{
-					_putchar('0');
+					_putchar(a * b / 100 + '0');
+					_putchar((a * b % 100) / 10 + '0');
+				}
+				if ((a * b) < 10)
+					_putchar(' ');
+				_putchar(a * b % 10 + '0');
+				if (b != n)
+				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				else
-				{
-					if ((a * b) < 100)
-						_putchar(' ');
-					if ((a * b) >= 10 && (a * b) < 100)
-						_putchar(a * b / 10 + '0');
-					else if ((a * b) >= 100)
-					{
-						_putchar(a * b / 100 + '0');
-						_putchar((a * b % 100) / 10 + '0');
-					}
-					if ((a * b) < 10)
-						_putchar(' ');
-					_putchar(a * b % 10 + '0');
-					if (b != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
