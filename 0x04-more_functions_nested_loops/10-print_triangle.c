59f21d1a591bb14b2bd61d57d9824e949e2589fe
Tr3v1n4t0r <813@holbertonschool.com>
2019-09-26 09:09:21 -0700

    made triangles happen

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *print_triangles - prints triangles of size 'size'
+ *
+ *@size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = size; a > 0; a--)
+		{
+			for (b = 0; b < a - 1; b++)
+				_putchar(' ');
+			for (c = size; c >= a; c--)
+				_putchar('#');
+			_putchar('\n');
+		}
+
+	}
+}
