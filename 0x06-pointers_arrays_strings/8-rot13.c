60ec0856158e3262b53b0acd270ecb628b80fa5d
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-03 13:42:18 -0700

    added capital letters

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -10,10 +10,8 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char start[] = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s,
-t, u, v, w, x, y, z";
-	char rotate[] = "n, o, p, q, r, s, t, u, v, w, x, y, z, a, b, c, d, e, f,
-g, h, i, j, k, l, m";
+	char start[] = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+	char rotate[] = "n, o, p, q, r, s, t, u, v, w, x, y, z, a, b, c, d, e, f, g, h, i, j, k, l, m, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, A, B, C, D, E, F, G, H, I, J, K, L, M";
 
 	for (i = 0; s[i]; i++)
 	{
