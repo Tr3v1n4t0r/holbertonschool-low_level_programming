01c0a964f0ea1eef6153b97ab7685d2ddb8b7e49
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-03 13:31:31 -0700

    svavfurq ebg13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *rot13 - encodes with rot13
+ *
+ *@s: pointer
+ *
+ *Return: char
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char start[] = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+	char rotate[] = "n, o, p, q, r, s, t, u, v, w, x, y, z, a, b, c, d, e, f, g, h, i, j, k, l, m";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; start[j]; j++)
+		{
+			if (s[i] == start[j])
+			{
+				s[i] = rotate[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
