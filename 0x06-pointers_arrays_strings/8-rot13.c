c761488e9573612b37904014ccec11b6dc4c1879
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-03 13:34:23 -0700

    znqr orggl unccl

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -10,8 +10,10 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char start[] = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-	char rotate[] = "n, o, p, q, r, s, t, u, v, w, x, y, z, a, b, c, d, e, f, g, h, i, j, k, l, m";
+	char start[] = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s,
+t, u, v, w, x, y, z";
+	char rotate[] = "n, o, p, q, r, s, t, u, v, w, x, y, z, a, b, c, d, e, f,
+g, h, i, j, k, l, m";
 
 	for (i = 0; s[i]; i++)
 	{
