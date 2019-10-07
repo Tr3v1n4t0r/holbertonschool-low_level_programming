cffb026fea602d66e10b020f9e36d230d54df11a
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-07 13:29:54 -0700

    added edge case

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,9 +10,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	for (; *s != '\0'; ++s)
+	for (; *s; s++)
 		if (*s == c)
 			return (s);
-	s++;
-	return (s);
+	if (*s == c)
+		return (s)
+	return ('\0');
 }
