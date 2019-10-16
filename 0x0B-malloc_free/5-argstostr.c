e83145056386b1720b5331f76d26cc1019f6afa8
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-15 17:56:22 -0700

    and dusted

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  *argstostr - concatonates all arguments
@@ -10,5 +11,41 @@
  */
 char *argstostr(int ac, char **av)
 {
+	int i, j, k = 0, length;
+	char *temp;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0, length = 0; i < ac; i++)
+		length += _strlen(av[i]);
+
+	temp = malloc(sizeof(char) * (length + ac + 1));
+	if (temp == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			temp[k++] = av[i][j];
+		temp[k++] = '\n';
+	}
+	temp[k] = '\0';
+	
+	return (temp);
+}
+
+/**
+ *_strlen - gets the length of a string
+ *
+ *@s: string
+ *
+ *Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
 }
