74e4828d4f4c9418d52471e7b6ea2ad9f1d25342
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-15 17:29:49 -0700

    repositioned NULL case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,14 +14,15 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *temp;
 
-	temp = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
-	if (temp == NULL)
-		return (NULL);
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	temp = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+	if (temp == NULL)
+		return (NULL);
+
 	for (i = 0; *s1; i++, s1++)
 		temp[i] = *s1;
 	for (; *s2; i++, s2++)
