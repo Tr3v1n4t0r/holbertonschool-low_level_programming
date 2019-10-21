b3ba84b3058d8d94742d468082e869f554bf6961
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-21 13:17:40 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - contains a dog
+ *
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
