ca8480921bd9dcb4d5a295a1d60913275c50ad4e
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-19 17:29:56 -0800

    betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,7 +12,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash = 5381;
 	int a;
 
-	while((a = *str++))
+	while ((a = *str++))
 		hash = ((hash << 5) + hash) + a;
 
 	return (hash);
