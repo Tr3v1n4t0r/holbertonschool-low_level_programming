cb2511648652cfbb842904ca560b2c6762dd86a0
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-19 18:08:31 -0800

    compiler ready

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,7 +8,7 @@
  *
  *Return: The value associated with the key, or NULL
  */
-char *hash_table_get(const hash_table_t *ht, const cahr *key)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *temp = NULL;
