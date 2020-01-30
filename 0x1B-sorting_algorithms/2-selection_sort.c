160a56cc306364de6bf8c421b8984bdb54001c19
Tr3v1n4t0r <813@holbertonschool.com>
2020-01-30 09:58:01 -0800

    base parts of files added

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ *selection_sort - Sorts an array of integers using the selection sort algorithm
+ *
+ *@array: The array
+ *@size: The size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+
+}
